ec317c22a797d809bec59055b66d9f6de514a2c3
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-06-02 10:50:31 +0100

    Update 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+a = len(argv)
+i == 1
+if a == 1
+    print(a, "argument:")
+elif a > 1
+    print(a, "arguments:")
+for i in range(len(argv)):
+    print(i, " : ", argv[i]\n)
