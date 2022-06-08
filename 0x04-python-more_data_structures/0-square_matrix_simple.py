9455fad717f103b475eae4b5a6de3cc0427f36d5
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-06-08 18:31:51 +0100

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for ind_a in matrix:
-            new_matrix[len(new_matrix):] = [[elem ** 2 for elem in ind_a]]
+            new_matrix[len(matrix):] = [[elem ** 2 for elem in ind_a]]
     return new_matrix
