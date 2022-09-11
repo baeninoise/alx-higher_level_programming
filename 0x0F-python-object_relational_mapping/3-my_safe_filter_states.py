bd4aa79edb27dea7fc135f3aeaadc92d2a3c185f
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-09-11 19:47:39 +0100

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Safe from SQL injections.
-# Usage: ./3-my_safe_filter_states.py <mysql username> \
-#                                     <mysql password> \
-#                                     <database name> \
-#                                     <state name searched>
-import sys
-import MySQLdb
-
+""" Script  that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument preventing SQL
+injection.
+"""
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
+    import sys
+    import MySQLdb
+
+    serv = MySQLdb.connect(host="localhost",  port=3306,
+                           user=sys.argv[1], password=sys.argv[2],
+                           database=sys.argv[3])
+
+    c = serv.cursor()
+    stateName = sys.argv[4]
+    c.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC", (
+        stateName,))
+    rows = c.fetchall()
+    for row in rows:
+        if row[1] == stateName:
+            print(row)
+    c.close()
+    serv.close()
